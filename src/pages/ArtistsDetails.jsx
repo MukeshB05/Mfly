3fd954cd22fe7f8ac89b71897487f9591b5d1e4b
Mukesh <mukesh2305b@gmail.com>
2026-02-27 23:27:10 +0530

    Fix API URL in ArtistsDetails component

--- src/pages/ArtistsDetails.jsx
+++ src/pages/ArtistsDetails.jsx
@@ -45,7 +45,7 @@ const ArtistsDetails = () => {
         let response = await fetch(`https://jiosaavn-api-lime.vercel.app/api/artists?id=${id}`);
         
          if (!response.ok) {
-          response = await fetch(`https://saavn.sumit.co/api/artists?id=${id}`);
+          response = await fetch(`https://jiosaavn-api-lime.vercel.app/api/artists?id=${id}`);
         }
   
         if (!response.ok) {
