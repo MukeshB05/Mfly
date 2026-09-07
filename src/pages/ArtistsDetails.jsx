77002b968813e726085cf7f82644723cd6cb4f50
Mukesh <mukesh2305b@gmail.com>
2026-09-07 23:58:54 +0530

    Update ArtistsDetails.jsx

--- src/pages/ArtistsDetails.jsx
+++ src/pages/ArtistsDetails.jsx
@@ -42,10 +42,10 @@ const ArtistsDetails = () => {
   useEffect(() => {
     const fetchDetails = async () => {
       try {
-        let response = await fetch(`https://shnwazdev-jiosaavn-apii.vercel.app/api/artists?id=${id}`);
+        let response = await fetch(`https://jiosaavn-api-black.vercel.app/api/artists?id=${id}`);
         
          if (!response.ok) {
-          response = await fetch(`https://shnwazdev-jiosaavn-apii.vercel.app/api/artists?id=${id}`);
+          response = await fetch(`https://jiosaavn-api-black.vercel.app/api/artists?id=${id}`);
         }
   
         if (!response.ok) {
